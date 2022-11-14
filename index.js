69581bdbf4ff4e91636d5874f6fdac1de3193aa8
Diego-Fdez <dieghoxcr7@gmail.com>
2022-11-14 13:49:09 -0600

    tarea 02

---
+++ index.js
@@ -0,0 +1,12 @@
+const nombre = 'Diego Fernandez';
+const edad = 35;
+const esDesarrollador = true;
+const fechaNacimiento = new Date(1987, 09, 27);
+const libroFavorito = [
+  {
+    titulo: 'El simbolo perdido',
+    autor: 'Dan Brown',
+    fecha: new Date(2009, 08, 15),
+    url: 'https://www.amazon.com/-/es/Dan-Brown/dp/6070706129',
+  },
+];
